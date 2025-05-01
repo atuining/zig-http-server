8f434dab0e44f9fef4517b62528099adee16fee4
atuining <nagpalpranay@gmail.com>
2025-05-01 20:07:11 +0530

    implementation for basic GET requests

---
+++ src/main.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const SocketConf = @import("config.zig");
+const Request = @import("request.zig");
+const Response = @import("response.zig");
+const Method = Request.Method;
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    const socket = try SocketConf.Socket.init();
+    try stdout.print("Server Addr: {any}\n", .{socket._address});
+    var server = try socket._address.listen(.{});
+    try stdout.print("Listening on port 3490\n", .{});
+    const connection = try server.accept();
+    var buffer: [1000]u8 = undefined;
+    for (0..buffer.len) |i| {
+        buffer[i] = 0;
+    }
+    try Request.read_request(connection, buffer[0..buffer.len]);
+
+    const request = Request.parse_request(buffer[0..buffer.len]);
+    if (request.method == Method.GET) {
+        if (std.mem.eql(u8, request.uri, "/")) {
+            try Response.send_200(connection);
+        } else {
+            try Response.send_400(connection);
+        }
+    }
+}
